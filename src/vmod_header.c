79d666df6c4c4f67f82b3e3c053ae8e342b3010d
Kristian Lyngstol <kristian@bohemians.org>
2011-05-10 11:47:38 +0200

    Add header-data and dummy-code

--- src/vmod_header.c
+++ src/vmod_header.c
@@ -1,3 +1,31 @@
+/*-
+ * Copyright (c) 2011 Varnish Software AS
+ * All rights reserved.
+ *
+ * Author: Kristian Lyngstol <kristian@bohemians.org>
+ *
+ * Redistribution and use in source and binary forms, with or without
+ * modification, are permitted provided that the following conditions
+ * are met:
+ * 1. Redistributions of source code must retain the above copyright
+ *    notice, this list of conditions and the following disclaimer.
+ * 2. Redistributions in binary form must reproduce the above copyright
+ *    notice, this list of conditions and the following disclaimer in the
+ *    documentation and/or other materials provided with the distribution.
+ *
+ * THIS SOFTWARE IS PROVIDED BY THE AUTHOR AND CONTRIBUTORS ``AS IS'' AND
+ * ANY EXPRESS OR IMPLIED WARRANTIES, INCLUDING, BUT NOT LIMITED TO, THE
+ * IMPLIED WARRANTIES OF MERCHANTABILITY AND FITNESS FOR A PARTICULAR PURPOSE
+ * ARE DISCLAIMED.  IN NO EVENT SHALL AUTHOR OR CONTRIBUTORS BE LIABLE
+ * FOR ANY DIRECT, INDIRECT, INCIDENTAL, SPECIAL, EXEMPLARY, OR CONSEQUENTIAL
+ * DAMAGES (INCLUDING, BUT NOT LIMITED TO, PROCUREMENT OF SUBSTITUTE GOODS
+ * OR SERVICES; LOSS OF USE, DATA, OR PROFITS; OR BUSINESS INTERRUPTION)
+ * HOWEVER CAUSED AND ON ANY THEORY OF LIABILITY, WHETHER IN CONTRACT, STRICT
+ * LIABILITY, OR TORT (INCLUDING NEGLIGENCE OR OTHERWISE) ARISING IN ANY WAY
+ * OUT OF THE USE OF THIS SOFTWARE, EVEN IF ADVISED OF THE POSSIBILITY OF
+ * SUCH DAMAGE.
+ */
+
 #include <stdlib.h>
 
 #include "vrt.h"
@@ -11,8 +39,31 @@ init_function(struct vmod_priv *priv, const struct VCL_conf *conf)
 	return (0);
 }
 
-int
-vmod_abs(struct sess *sp, int val)
+void __match_proto__()
+vmod_append(struct sess *sp, enum gethdr_e e, const char *h, const char *s)
 {
-	return (abs(val));
+	(void)e;
+	(void)sp;
+	(void)h;
+	return (1);
 }
+
+/*
+void __match_proto__()
+vmod_remove(struct sess *sp, enum gethdr_e e, const char *h, const char *s)
+{
+	(void)e;
+	(void)sp;
+	(void)h;
+	return (1);
+}
+
+void __match_proto__()
+vmod_get(struct sess *sp, enum gethdr_e e, const char *h, const char *s)
+{
+	(void)e;
+	(void)sp;
+	(void)h;
+	return (1);
+}
+*/
